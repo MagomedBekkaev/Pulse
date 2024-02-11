b5e9c6b4feea08ce3c19b02d54df4a5cb22115d4
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2024-02-11 22:03:01 +0100

    styiling home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ForumRepository;
 
 class HomeController extends AbstractController
 {
+
+    private $forumRepository;
+
+    public function __construct(ForumRepository $forumRepository)
+    {
+        $this->forumRepository = $forumRepository;
+    }
+
+     #[Route('/', name: 'app_home')]
+    public function showForum(): Response
+    {
+        $forums = $this->forumRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'forums' => $forums,
+        ]);
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
